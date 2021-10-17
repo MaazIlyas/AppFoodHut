3d09af9d5048a734d3c24684ca25a252026923c4
MaazIlyas <maazilyas@yahoo.com>
2021-10-17 16:52:28 +0930

    Fixed merge issue

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image, SafeAreaView, StyleSheet, TextInput, FlatList } from "react-native";
+import { View, Text,TouchableOpacity, Image, SafeAreaView, StyleSheet, TextInput, FlatList } from "react-native";
 import { COLORS, FONTS, icons, images, SIZES } from "../constants";
 
 
 const Home = () => {
 
     //Writing Dummy Data
-    const categoryData = [{
+    const categoryData = [
+        {
             id: 1,
             name: "All",
             icon: icons.allFV,
@@ -33,7 +34,8 @@ const Home = () => {
     const fairPrice = 2
     const expensive = 3
 
-    const restaurantData = [{
+    const restaurantData = [
+        {
             id: 1,
             name: "Burger Crisp",
             rating: 4.8,
@@ -57,7 +59,7 @@ const Home = () => {
     const [categories, setCategories] = React.useState(categoryData)
     const [selectedCategory, setSelectedCategory] = React.useState(null)
     const [restaurants, setRestaurants] = React.useState(restaurantData)
-        // const [currentLocation, setCurrentLocation] = React.useState(initialCurrentLocation)
+    // const [currentLocation, setCurrentLocation] = React.useState(initialCurrentLocation)
 
 
     function onSelectCategory(category) {
@@ -70,155 +72,133 @@ const Home = () => {
     }
 
     function renderSearch() {
-        return ( <
-            View style = {
-                {
-                    flexDirection: 'row',
-                    height: 40,
-                    alignItems: 'center',
-                    marginHorizontal: SIZES.base,
-                    marginVertical: SIZES.base,
-                    paddingHorizontal: SIZES.radius,
-                    borderRadius: SIZES.radius,
-                    backgroundColor: COLORS.lightGray
+        return(
+            <View
+            style={{
+                flexDirection: 'row',
+                height: 40,
+                alignItems: 'center',
+                marginHorizontal: SIZES.base,
+                marginVertical: SIZES.base,
+                paddingHorizontal: SIZES.radius,
+                borderRadius: SIZES.radius,
+                backgroundColor: COLORS.lightGray
 
-                }
-            } >
-            { /* Icon */ } <
-            Image source = { icons.search }
-            style = {
-                {
-                    height: 20,
-                    width: 20,
-                    tintColor: COLORS.black
-                }
-            }
-            /> { /* Text ibput */ } <
-            TextInput style = {
-                {
+            }}
+            >
+            {/* Icon */}
+            <Image
+            source={icons.search}
+            style={{
+                height: 20,
+                width: 20,
+                tintColor: COLORS.black
+            }}
+            />
+            {/* Text ibput */}
+            <TextInput
+                style={{
                     flex: 1,
                     marginLeft: SIZES.radius,
-                }
-            }
-            placeholder = "Search food...." /
-            > { /* Filter Buttons */ } <
-            TouchableOpacity
+                }}
+                placeholder="Search food...."
+            />
+            {/* Filter Buttons */}
+            <TouchableOpacity
             //onPress - will work later on
             >
-            <
-            Image source = { icons.hamburger }
-            style = {
-                {
+                <Image
+                source={icons.hamburger}
+                style={{
                     height: 20,
                     width: 20,
                     tintColor: COLORS.black
-                }
-            }
-            /> <
-            /TouchableOpacity> <
-            /View>
+                }}
+                />
+            </TouchableOpacity>
+        </View>
 
         )
     }
-
-    <<
-    << << < HEAD
-        ===
-        === =
-        // Render Categories
-        function renderFoodPreference() {
-            const renderItem = ({ item }) => {
-                return ( <
-                    TouchableOpacity style = {
-                        {
-                            padding: SIZES.padding + 1.5,
-                            paddingBottom: SIZES.padding * 2,
-                            backgroundColor: (selectedCategory ? .id == item.id) ? COLORS.primary : COLORS.white,
-                            borderRadius: SIZES.radius,
-                            alignItems: "center",
-                            justifyContent: "center",
-                            marginRight: SIZES.padding,
-                        }
-                    }
-                    onPress = {
-                        () => onSelectCategory(item) } >
-                    <
-                    View style = {
-                        {
+        
+    // Render Categories
+    function renderFoodPreference() {
+        const renderItem = ({ item }) => {
+            return (
+                <TouchableOpacity
+                    style={{
+                        padding: SIZES.padding +1.5,
+                        paddingBottom: SIZES.padding * 2,
+                        backgroundColor: (selectedCategory?.id == item.id) ? COLORS.primary : COLORS.white,
+                        borderRadius: SIZES.radius,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        marginRight: SIZES.padding,
+                    }}
+                    onPress={() => onSelectCategory(item)}
+                >
+                    <View
+                        style={{
                             width: 50,
                             height: 50,
                             borderRadius: 25,
                             alignItems: "center",
                             justifyContent: "center",
-                            backgroundColor: (selectedCategory ? .id == item.id) ? COLORS.white : COLORS.lightGray
-                        }
-                    } >
-                    <
-                    Image source = { item.icon }
-                    resizeMode = "contain"
-                    style = {
-                        {
-                            width: 30,
-                            height: 30
-                        }
-                    }
-                    /> <
-                    /View>
+                            backgroundColor: (selectedCategory?.id == item.id) ? COLORS.white : COLORS.lightGray
+                        }}
+                    >
+                        <Image
+                            source={item.icon}
+                            resizeMode="contain"
+                            style={{
+                                width: 30,
+                                height: 30
+                            }}
+                        />
+                    </View>
 
-                    <
-                    Text style = {
-                        {
+                    <Text
+                        style={{
                             marginTop: SIZES.padding,
-                            color: (selectedCategory ? .id == item.id) ? COLORS.white : COLORS.black,
-                        }
-                    } >
-                    { item.name } <
-                    /Text> <
-                    /TouchableOpacity>
-                )
-            }
-
-            return ( <
-                View style = {
-                    { padding: SIZES.padding * 2 } } >
-                <
-                Text style = {
-                    {...FONTS.h1 } } > Food < /Text> <
-                Text style = {
-                    {...FONTS.h1 } } > Preference < /Text>
-
-                <
-                FlatList data = { categories }
-                horizontal showsHorizontalScrollIndicator = { false }
-                keyExtractor = { item => `${item.id}` }
-                renderItem = { renderItem }
-                contentContainerStyle = {
-                    { paddingVertical: SIZES.padding * 2 } }
-                /> <
-                /View>
+                            color: (selectedCategory?.id == item.id) ? COLORS.white : COLORS.black,
+                        }}
+                    >
+                        {item.name}
+                    </Text>
+                </TouchableOpacity>
             )
         }
 
+        return (
+            <View style={{ padding: SIZES.padding * 2 }}>
+                <Text style={{ ...FONTS.h1 }}>Food</Text>
+                <Text style={{ ...FONTS.h1 }}>Preference</Text>
 
-    >>>
-    >>> > Home - Categories
+                <FlatList
+                    data={categories}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={renderItem}
+                    contentContainerStyle={{ paddingVertical: SIZES.padding * 2 }}
+                />
+            </View>
+        )
+    }
+
+
     return (
 
-        <
-        View style = {
-            {
-                flex: 1
-            }
-        } >
-        <<
-        << << < HEAD { /* Search */ } { renderSearch() } ===
-        === = { /* Move it to a function after getting to know the way */ } { /* Search */ } { renderSearch() }
+        <View style={{
+            flex:1
+        }}>
+            {/* Move it to a function after getting to know the way */}
+            {/* Search */}
+            {renderSearch()}
 
-        { renderFoodPreference() } >>>
-        >>> > Home - Categories
+            {renderFoodPreference()}
 
-        <
-        /View>
+        </View>
     )
 }
 
