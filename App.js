46898bfdaf5ef483e3eeb56427f2df4cdfbc98d1
MaazIlyas <maazilyas@yahoo.com>
2021-10-17 10:54:17 +0930

    File Structure
    File structure created with Navigation tabs

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native'
+
+import { Home, Reservation, Menu } from './screens'
+import Tabs from './navigation/tabs'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Home'}
+            >
+                <Stack.Screen name="Home" component={Tabs} />
+                <Stack.Screen name="Menu" component={Menu} />
+              <Stack.Screen name="Reservation" component={Reservation} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default App;
---
+++ screens/index.js
@@ -0,0 +1,11 @@
+import Home from './Home'
+import Cart from './Cart'
+import Reservation from './Reservation'
+import Menu from './Menu'
+
+export {
+    Home,
+    Reservation,
+    Cart,
+    Menu
+}
